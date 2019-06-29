5ca4261e33c16b6d906b28063ff391dc3f3a475c
Junghoon Yoon <juhuyoon@yahoo.com>
2019-06-29 16:56:56 -0400

    all the hbs needed, now just adding static content

--- controllers/html/html-routes.js
+++ controllers/html/html-routes.js
@@ -6,8 +6,50 @@ router.get('/', (req, res) => {
   res.render('home');
 });
 
-router.post('/burgers/create', (req, res) => {
-  db.create()
-})
+// For consultation apge
+router.get('/consultation', (req, res) => {
+  res.render('consultation');
+});
+
+// For Intro page
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+// For offices
+router.get('/seoul', (req, res) => {
+  res.render('office/seoul');
+});
+
+router.get('/atlanta', (req, res) => {
+  res.render('office/atlanta');
+});
+
+// For legal help
+
+router.get('/intro', (req, res) => {
+  res.render('legal/intro');
+});
+
+router.get('/contract', (req, res) => {
+  res.render('legal/contract');
+});
+
+router.get('/criminal', (req, res) => {
+  res.render('legal/criminal');
+});
+
+router.get('/family', (req, res) => {
+  res.render('legal/family');
+});
+
+router.get('/inheritance', (req, res) => {
+  res.render('legal/inheritance');
+});
+
+router.get('/taxes', (req, res) => {
+  res.render('legal/taxes');
+});
+
 
 module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,19 +1,27 @@
 $(document).ready(() => {
-  $('.devour-form').on('submit', function (event) {
-    event.preventDefault();
+  // $('.devour-form').on('submit', function (event) {
+  //   event.preventDefault();
 
-    var burgerInfo = {
-      burger_id: $(this).children('.burger_id').val(),
-      customer: $(this).children('.custom-input').val()
-    };
+  //   var burgerInfo = {
+  //     burger_id: $(this).children('.burger_id').val(),
+  //     customer: $(this).children('.custom-input').val()
+  //   };
 
-    $.ajax({
-      method: 'PUT',
-      url: '/burgers/update',
-      data: burgerInfo
-    }).then(function (data) {
-      // reload page to display devoured burger in proper column
-      location.reload();
-    });
-  });
+  //   $.ajax({
+  //     method: 'PUT',
+  //     url: '/burgers/update',
+  //     data: burgerInfo
+  //   }).then(function (data) {
+  //     // reload page to display devoured burger in proper column
+  //     location.reload();
+  //   });
+  // });
+  $('.dropdown-trigger').dropdown({ hover: true });
+  $('.carousel').carousel({fullWidth:true}).css('height', $(window).height());
+  autoplay();
 });
+
+const autoplay = () => {
+  $('.carousel').carousel('next');
+  setTimeout(autoplay, 4500);
+}

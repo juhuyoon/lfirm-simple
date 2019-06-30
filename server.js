0fdc2d411efbb63a98ad41c047234e23e8f21d99
Junghoon Yoon <juhuyoon@yahoo.com>
2019-06-29 20:26:50 -0400

    cors implemented

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const cors = require('cors');
 // bring in the models
 const db = require('./models/');
 const routes = require('./controllers/');
 
 const app = express();
+const allowedOrigins = ['http://localhost:3000'];
+
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static('public'));
+app.use(cors({
+
+  origin(origin, callback) {
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not '
+                + 'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+
+  exposedHeaders: ['Content-Length', 'X-Foo', 'X-Bar'],
+
+  credentials: true
+}), express.static('public'));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));

d10f82bf71df6c03d6d50e6431080c1f3978395a
Junghoon Yoon <juhuyoon@yahoo.com>
2019-07-05 00:29:32 -0400

    new frame style

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -25,3 +25,18 @@ const autoplay = () => {
   $('.carousel').carousel('next');
   setTimeout(autoplay, 4500);
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
